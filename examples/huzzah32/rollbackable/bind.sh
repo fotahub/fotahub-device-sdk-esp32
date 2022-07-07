6e38b3aecba7caea3ef2e436055ee9810b123689
FotaHub <>
2022-07-07 11:32:06 +0200

    FotaHub Device SDK for ESP32 1.1.0-14

--- examples/huzzah32/rollbackable/bind.sh
+++ examples/huzzah32/rollbackable/bind.sh
@@ -3,5 +3,5 @@ docker run --rm \
   --env HOST=Linux \
   --volume $PWD/../../..:/workspace \
   --workdir /workspace/examples/huzzah32/rollbackable \
-  fotahub/esp-idf:4.4-1 \
+  fotahub/esp-idf:4.4.1-1 \
   ./idf.sh build
--- examples/huzzah32/thingshadow/bind.bat
+++ examples/huzzah32/thingshadow/bind.bat
@@ -3,5 +3,5 @@ docker run --rm ^
   --env HOST=Windows ^
   --volume %~dp0..\..\..:/workspace ^
   --workdir /workspace/examples/huzzah32/thingshadow ^
-  fotahub/esp-idf:4.4-1 ^
+  fotahub/esp-idf:4.4.1-1 ^
   ./idf.sh build
